57f4650bfdda6f76d0776414a16ca64c585edc04
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-08-24 22:41:02 +0200

    Finish plugins chapter.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::build()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
+        .add_plugin(HelloPlugin)
         .run();
 }
 
@@ -35,5 +33,16 @@ fn add_people(mut commands: Commands) {
 fn greet_people(query: Query<&Name, With<Person>>) {
     for name in query.iter() {
         println!("hello, {}!", name.0)
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        // add things to your app here
+        app.add_startup_system(add_people.system())
+            .add_system(hello_world.system())
+            .add_system(greet_people.system());
     }
 }
