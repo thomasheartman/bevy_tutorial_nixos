bfad8263593d9fcef6486cce3e601d8fccc22b92
Thomas Heartman <thomas.heartman+git@gmail.com>
2021-08-24 22:46:27 +0200

    Finish Getting Started 🎉🥳

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,6 @@ fn main() {
         .run();
 }
 
-fn hello_world() {
-    println!("hello, world");
-}
-
 struct Person;
 
 struct Name(String);
@@ -30,9 +26,11 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello, {}!", name.0)
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello, {}!", name.0)
+        }
     }
 }
 
@@ -41,8 +39,10 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut AppBuilder) {
         // add things to your app here
-        app.add_startup_system(add_people.system())
-            .add_system(hello_world.system())
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
             .add_system(greet_people.system());
     }
 }
+
+struct GreetTimer(Timer);
